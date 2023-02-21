811ccd6eb44eef63397ddf4a623d84a785cf0a27
Mattias Kindborg <mattias.kindborg@axis.com>
2023-02-21 15:55:33 +0100

    refactor: input bindings

--- servicebusthumbnail/index.ts
+++ servicebusthumbnail/index.ts
@@ -1,30 +1,33 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import resizeImg from "resize-img"
 
-interface inputDocument {
+interface Message {
     id: string,
     uri: string,
-    thumbnail: string
 }
+
+interface Document extends Message {
+  thumbnail?: string
+}
+
 const serviceBusQueueTrigger: AzureFunction = async function (
   context: Context,
-  inputimage: inputDocument
+  message: Message,
+  document: Document,
+  image: Buffer
 ): Promise<void> {
   context.log.warn("AM I ALIVE?")
+
   const account = "victorimagestore";
-  const resizedImage = await resizeImg(context.bindings.inputimageblob, {
+  const resizedImage = await resizeImg(image, {
     width: 128, 
     height: 128
   })
 
-  const POST_BODY = {
-    ...inputimage,
-    thumbnail: `https://${account}.blob.core.windows.net/thumbnail/${inputimage.id}`,
-  };
-
-  context.bindings.outputimage = POST_BODY
-  context.bindings.outputimageblob = resizedImage
+  document.thumbnail = `https://${account}.blob.core.windows.net/thumbnail/${message.id}`
   
+  context.bindings.outputimage = document
+  context.bindings.outputimageblob = resizedImage
 };
 
 export default serviceBusQueueTrigger;
